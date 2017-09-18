48f6f1eafeef6a7158de5f9c3a0f677cac014153
Jacob Fredsøe <jcf@clin.au.dk>
2017-09-18 14:34:13 +0200

    Add project files.

---
+++ REDCapAPI/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REDCapAPI
+{
+    public class Class1
+    {
+    }
+}
